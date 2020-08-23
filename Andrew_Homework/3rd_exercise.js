cbf38eb01d110e3cf3c13b4a29bf1f48c1382c62
WatCodeDatCode <arussell742@gmail.com>
2020-08-23 14:27:04 +0200

    Add float input validation

--- Andrew_Homework/3rd_exercise.js
+++ Andrew_Homework/3rd_exercise.js
@@ -1,6 +1,6 @@
 let readlineSync = require('readline-sync');
 
-let amountDollar = readlineSync.question('Enter the amount you would like to conver to Euros: '); //Sets input of user as variabe for dollar
+let amountDollar = readlineSync.questionFloat('Enter the amount you would like to conver to Euros: '); //Sets input of user as variabe for dollar
 let exchangeEuro = 0.844; //Sets exchange rate for Euro for conversion
 let conversion = amountDollar * exchangeEuro;
 
