d44e8661a9cbbe7ddbb255863480afff4ee1d74c
Helder Pereira <phelder@gmail.com>
2020-08-26 21:45:09 +0200

    Update js6 - funPrime.js

--- code/lesson 5/js6 - funPrime.js
+++ code/lesson 5/js6 - funPrime.js
@@ -1,7 +1,6 @@
 // CREATE A FUNCTION THAT RECEIVES A NUMBER AS PARAMETER
 // AND RETURNS A BOOLEAN THAT TELLS YOU WETHER THE NUMBER IS PRIME OR NOT
 
-// print all the prime numbers between 1 and 100
 function prime(number) {
     if (number < 2) {
         return false;
@@ -14,19 +13,14 @@ function prime(number) {
     return true;
 }
 
+// print all the prime numbers between 1 and 100
 for (let i = 1; i <= 100; i++) {
     if (prime(i)) {
         console.log(i);
     }
 }
 
+
 // HOMEWORK:
 // NOW PRINT THE FIRST 100 PRIME NUMBERS!!!!!
-
-
-
-
-
-
-
 
