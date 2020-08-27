7f019ebd9e5b1db5f27dad3ce8b3daeec10b95f1
WatCodeDatCode <arussell742@gmail.com>
2020-08-27 12:08:56 +0200

    Add comments to exercise 12

--- Andrew_Homework/12th_exercise.js
+++ Andrew_Homework/12th_exercise.js
@@ -1,54 +1,28 @@
-// # Homework
-// - [Exercise 12](#exercise-12)
-
-// ## Exercise 12
-// ### Triangles
 // Write a programa that asks the user for a size `s` and then outputs a triangle with the height and width of `s`
-
-// > **Example:**<br />
-// > User inputs `4`<br />
-// > Output:
-// ```
-// *
-// **
-// ***
-// ****
-// ```
-// > **Bonus goal:**<br />
-// > Also ask the user for how many triangles to print.<br />
-// > User inputs size `3`<br />
-// > User inputs times`2`<br />
-// > Output:
-// ```
-// *
-// **
-// ***
-// *
-// **
-// ***
-// ```
 
 let readlineSync = require('readline-sync');
  
-// Ask user for size of triangle
+// Ask user for size and numer of triangles
 let size = readlineSync.questionInt('Enter the desired size (greater than 1) of the triangle: ');
 let amount = readlineSync.questionInt('Enter the number of triangles: ');
-let triangleRow = '';
+let triangleRow = ''; // Variable for logging triangle rows
 
+// Function for logging each triangle
 function triangle(size) {
     for (let i = 0; i < size; i++) {
-        triangleRow += '*';
+        triangleRow += '*'; // Add current iteration of row to variable and log to console
         console.log(triangleRow);
     }
-    return triangleRow = '';
+    return triangleRow = ''; // Resets variable to empty string for further triangles to be printed
 }
 
-function print(amount) {
+function print(amount) { // Function for passing through user amount through the triangle function
     for (let i= 0; i < amount; i++) {
-        triangle(size);
+        triangle(size); // For each iteration of "amount", calls triangle function for logging
     }
 }
 
+// If size and amount are too low, give error, otherwise call the print function
 if (size < 2 || amount < 1) {
     console.log('Size must be greater than 1 and amount must be at least 1!')
 } else {
