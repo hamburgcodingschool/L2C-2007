c177630fbfb82a9a7a1d4a39e69532680550d1bf
WatCodeDatCode <arussell742@gmail.com>
2020-08-29 10:29:36 +0200

    Improve formatting for multiplication table

--- Andrew_Homework/14th_exercise.js
+++ Andrew_Homework/14th_exercise.js
@@ -1,35 +1,34 @@
 // List a table that multiplies all the numbers from 1 to 10.
-// Format your output to something like this:
-
-//  1   2   3   4   5   6   7   8   9  10
-//  2   4   6   8  10  12  14  16  18  20
-//  3   6   9  12  15  18  21  24  27  30
-//  4   8  12  16  20  24  28  32  36  40
-//  5  10  15  20  25  30  35  40  45  50
-//  6  12  18  24  30  36  42  48  54  60
-//  7  14  21  28  35  42  49  56  63  70
-//  8  16  24  32  40  48  56  64  72  80
-//  9  18  27  36  45  54  63  72  81  90
-// 10  20  30  40  50  60  70  80  90 100
-let multiplicationTable = '';
+let multiplicationTable = '\n';
 
 function multiply(number) {
     for (i = 1; i <= 10; i++) {
-        multiplicationTable += (number * i) + ' ';
+        let formatter = i * number; // Variable for adding spaces before numbers to improve readability
+        // Numbers below 10 will have 2 spaces before iteration, below 100 only 1 space
+        if (formatter < 10) {
+            multiplicationTable += ' ';
+        }
+
+        if (formatter < 100) {
+            multiplicationTable += ' ';
+        }
+        // Add current number from formatter to table
+        multiplicationTable += formatter + ' ';
 
         if (i == 10) {
-            multiplicationTable += '\n';
+            multiplicationTable += '\n'; // Once iteration is ended, add space for next line
         }
     }
-    return multiplicationTable;
+    return multiplicationTable; // Returns mulitplication table for logging later
 }
 
-let sizeOfTable = 10;
-function table(sizeOfTable) {
-    for (j = 1; j <= sizeOfTable; j++) {
+// Function for passing through the size of table to multiplication table function
+function table(rows) {
+    for (j = 1; j <= rows; j++) {
         multiply(j);
     }
     return multiplicationTable;
 }
 
-console.log(table(sizeOfTable));
+let sizeOfTable = 10; // Variable for number of rows multiplications table should have
+console.log(table(sizeOfTable)); // Use size variable to call and log table
