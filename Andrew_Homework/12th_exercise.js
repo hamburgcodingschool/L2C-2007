915bb6947efe1a655abff711c6d578bb0b6ffa34
WatCodeDatCode <arussell742@gmail.com>
2020-08-29 09:09:35 +0200

    Improve functions of exercise 12

--- Andrew_Homework/12th_exercise.js
+++ Andrew_Homework/12th_exercise.js
@@ -1,5 +1,4 @@
 // Write a programa that asks the user for a size `s` and then outputs a triangle with the height and width of `s`
-
 let readlineSync = require('readline-sync');
  
 // Ask user for size and numer of triangles
@@ -7,25 +6,25 @@ let size = readlineSync.questionInt('Enter the desired size (greater than 1) of 
 let amount = readlineSync.questionInt('Enter the number of triangles: ');
 let triangleRow = ''; // Variable for logging triangle rows
 
-// Function for logging each triangle
+// Function for creating triangle
 function triangle(size) {
-    for (let i = 0; i < size; i++) {
-        triangleRow += '*'; // Add current iteration of row to variable and log to console
-        console.log(triangleRow);
+    for (let i = 1; i <= size; i++) {
+        triangleRow += '\n'; // Add space before each new row 
+        triangleRow += '*'.repeat(i); // Repeat star by number of current iteration, important for use of numOfTriangles
     }
-    return triangleRow = ''; // Resets variable to empty string for further triangles to be printed
+    return triangleRow; 
 }
 
-// Function for passing through user amount through the triangle function
 function numOfTriangles(amount) { 
-    for (let i= 0; i < amount; i++) {
-        triangle(size); // For each iteration of "amount", calls triangle function for logging
+    for (let i= 1; i < amount; i++) {
+        triangle(size); // For each iteration of "amount", calls triangle function
     }
+    return triangle(size); // Returns the triangle function after the for loop
 }
 
-// If size and amount are too low, give error, otherwise call the print function
+// If size and amount are too low, give error, otherwise log the called numOfTriangles function
 if (size < 2 || amount < 1) {
-    console.log('Size must be greater than 1 and amount must be at least 1!')
+    console.log('Size of triangle must be greater than 1 and number of triangles must be at least 1!')
 } else {
-    numOfTriangles(amount);
+    console.log(numOfTriangles(amount));
 }
