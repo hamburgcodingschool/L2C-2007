edb1cb809559ff7b0ff81a5e65da20191b9698c6
Helder Pereira <helder.pereira@xing.com>
2020-08-31 21:05:24 +0200

    update lesson 3

--- code/lesson 7/js2 - matrix_calc.js
+++ code/lesson 7/js2 - matrix_calc.js
@@ -24,16 +24,81 @@ function generateNumberMatrix(size, min, max) {
     return newMatrix;
 }
 
-let matrix1 = generateNumberMatrix(4, 1, 9);
+function sumOfRowsInMatrix(matrix) {
+    let sumArray = [];
+    for (let i = 0; i < matrix.length; i++) {
+        let total = 0;
+        for (let j = 0; j < matrix[i].length; j++) {
+            total += matrix[i][j];
+        }
+        sumArray.push(total);
+    }
+    return sumArray;
+}
 
+function sumOfColsInMatrix(matrix) {
+    let sumArray = [];
+    for (let i = 0; i < matrix.length; i++) {
+        let total = 0;
+        for (let j = 0; j < matrix[i].length; j++) {
+            total += matrix[j][i];
+        }
+        sumArray.push(total);
+    }
+    return sumArray;
+}
+
+let matrix1 = generateNumberMatrix(4, 1, 3);
+let matrix1RowSum = sumOfRowsInMatrix(matrix1);
+let matrix1ColSum = sumOfColsInMatrix(matrix1);
+
+
+// PRINT ALL THE LINES OF THE MATRIX AND RESPECTIVE LINE TOTAL
 for (let i = 0; i < matrix1.length; i++) {
     let tempString = '';
+
     for (let j = 0; j < matrix1[i].length; j++) {
         if (j > 0) {
             tempString += ' + ';
         }
         tempString += matrix1[i][j];
     }
-    console.log(tempString);
+    console.log(tempString + ' = ' + matrix1RowSum[i]);
 }
 
+// DO A FOR LOOP LATER
+console.log('=   =   =   =');
+
+let tempString = '';
+for (let i = 0; i < matrix1ColSum.length; i++) {
+    tempString += matrix1ColSum[i] + '   ';    
+}
+console.log(tempString);
+
+
+// for (let i = 0; i < matrix1.length; i++) {
+//     let tempString = '';
+
+//     let total = 0;
+//     for (let j = 0; j < matrix1[i].length; j++) {
+//         total += matrix1[i][j];
+//         if (j > 0) {
+//             tempString += ' + ';
+//         }
+//         tempString += matrix1[i][j];
+//     }
+//     console.log(tempString + ' = ' + total);
+// }
+
+// console.log('=   =   =   =');
+
+// let tempString = '';
+// for (let i = 0; i < matrix1.length; i++) {
+//     let total = 0;
+//     for (let j = 0; j < matrix1[i].length; j++) {
+//         total += matrix1[j][i];
+//     }
+//     tempString += total + '   ';
+// }
+
+// console.log(tempString);
