f4d71776010a65ace1b43799a23adb2c2f1c7c13
WatCodeDatCode <arussell742@gmail.com>
2020-09-01 20:10:30 +0200

    Improve loop conditional statement

--- Andrew_Homework/uniqueArray.js
+++ Andrew_Homework/uniqueArray.js
@@ -9,9 +9,7 @@ let counter = 0;
 while (counter < 6) { // Set counter to how many numbers should be included in array
     let x = random(1, 49); // Calculate a random number between 1 and 49 (range should always be equal or greater than desired array!)
 
-    if (array.includes(x)) { // If x is already in the array, continue 
-        continue;
-    } else {
+    if (!array.includes(x)) { // If x is already in the array, continue 
         array[counter] = x; // Otherwise set the current array iteration using counter to x
         counter++; // Increase counter by 1 for the next iteration until array size reached
     }
